c8a3944f99b955d2073cb29f0a376a52911f9502
Raekwon Adams-Rodgers <adamsrodgers@etsu.edu>
2022-12-18 00:24:18 -0500

    Added in footer with a little design (to be changed)

--- SpartElite/SpartElite/Site1.Master.designer.cs
+++ SpartElite/SpartElite/Site1.Master.designer.cs
@@ -76,5 +76,14 @@ namespace SpartElite
 		/// To modify move field declaration from designer file to code-behind file.
 		/// </remarks>
 		protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceholder1;
+
+		/// <summary>
+		/// adminLog control.
+		/// </summary>
+		/// <remarks>
+		/// Auto-generated field.
+		/// To modify move field declaration from designer file to code-behind file.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.LinkButton adminLog;
 	}
 }
