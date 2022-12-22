9f3d47523877c5a88643957b1b0dd1d097099146
Raekwon Adams-Rodgers <adamsrodgers@etsu.edu>
2022-12-21 23:57:03 -0500

    Formatted Footer better

--- SpartElite/SpartElite/Site1.Master.designer.cs
+++ SpartElite/SpartElite/Site1.Master.designer.cs
@@ -33,6 +33,15 @@ namespace SpartElite
 		protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
 		/// <summary>
+		/// DashboardButton control.
+		/// </summary>
+		/// <remarks>
+		/// Auto-generated field.
+		/// To modify move field declaration from designer file to code-behind file.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.LinkButton DashboardButton;
+
+		/// <summary>
 		/// signUpButton control.
 		/// </summary>
 		/// <remarks>
